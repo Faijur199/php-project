56af5a6777530f2e1c7fb34a70ee0b19802fdc0c
Faijur199 <faijurahaman4@gmail.com>
2025-06-26 07:16:42 +0545

    initial commit

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<nav class="bg-gray-800">
+  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div class="relative flex h-16 items-center justify-between">
+      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+      </div>
+      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+        <div class="flex shrink-0 items-center">
+          <img class="h-9 w-auto  rounded-3xl" src="./profilepic.png" alt="Your Company" />
+        </div>
+        <div class="hidden sm:ml-6 sm:block">
+          <div class="flex space-x-4">
+            <a href="index.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">HOME</a>
+            <a href="product.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Product</a>
+            <a href="about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+            <a href="contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+          </div>
+        </div>
+        <div class="flex justify-end w-full gap-x-1">
+            <input class=" bg-white rounded-2xl px-3" type="search" placeholder="Search...">
+            <a href="#" class="bg-white px-2 py-1 rounded-xl hover:bg-gray-700 hover:text-white">Search</a>
+        </div>
+      </div>
+
+</nav>
